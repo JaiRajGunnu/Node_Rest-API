eb3008bd03e0a5e522da2b4ab0a0cfd80b4a8a17
JaiRajGunnu <jairajgsklm@gmail.com>
2023-09-09 16:07:26 +0530

    cc

--- server.js
+++ server.js
@@ -1,48 +1,19 @@
-const express = require('express');
-const fs = require('fs');
-const app = express();
-const port = 8080;
-
-// Middleware to parse JSON requests
-app.use(express.json());
-
-// Sample data (users)
-const users = [
-  { id: 1, name: 'John Doe' },
-  { id: 2, name: 'Jane Smith' },
-];
+//REST API demo in Node.js
+var express = require('express'); // requre the express framework
+var app = express();
+var fs = require('fs'); //require file system object
 
 // Endpoint to Get a list of users
-app.get('/getUsers', (req, res) => {
-  res.json(users);
-});
+app.get('/getUsers', function(req, res){
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+        console.log(data);
+        res.end(data); // you can also use res.send()
+    });
+})
 
-// Endpoint to Get a single user by ID
-app.get('/getUser/:id', (req, res) => {
-  const userId = parseInt(req.params.id);
-  const user = users.find((u) => u.id === userId);
-
-  if (!user) {
-    res.status(404).json({ message: 'User not found' });
-  } else {
-    res.json(user);
-  }
-});
-
-// Endpoint to Create a new user
-app.post('/createUser', (req, res) => {
-  const { name } = req.body;
-
-  if (!name) {
-    res.status(400).json({ message: 'Name is required' });
-  } else {
-    const newUser = { id: users.length + 1, name };
-    users.push(newUser);
-    res.status(201).json(newUser);
-  }
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`REST API demo app listening at http://localhost:${port}`);
-});
+// Create a server to listen at port 8080
+var server = app.listen(8000, function(){
+    var host = server.address().address
+    var port = server.address().port
+    console.log("REST API demo app listening at http://%s:%s", host, port)
+})
